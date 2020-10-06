a1c143bf0de1c3e22846504e374ab23f4bedbfac
smj6052 <seominjung6052@gmail.com>
2020-10-06 23:58:58 +0900

    changmo

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    cout<<"아니아니 수건좀 달라니까"<<endl;
+    
+    cout<<"창모"<<endl;
+    cout<<"널 지워야해"<<endl;
+    cout<<"내가 니놈이었다면, 그렇지 않지만 우린 마시러 갔을 꺼야, 덕소 앨리팝";
+}
